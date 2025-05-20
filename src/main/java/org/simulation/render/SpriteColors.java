60a0bc6dcb6496762a58866f7f5a52f3e05bdb7a
dmitry <dmitryed6263@gmail.com>
2025-05-20 21:56:40 +0400

    added sprites as a separate class

---
+++ src/main/java/org/simulation/render/SpriteColors.java
@@ -0,0 +1,16 @@
+package org.simulation.render;
+
+public class SpriteColors {
+
+    public static final String ANSI_BACKGROUND_GREEN_COLOR = "\u001B[42m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String LIGHT_GREEN = "\u001B[92m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String GRAY = "\u001B[90m";
+    public static final String WHITE = "\u001B[37m";
+
+
+
+}
