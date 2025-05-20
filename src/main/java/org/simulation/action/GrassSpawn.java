c72b93b057e9113f3ad25b5a47f36d777b1ec20c
dmitry <dmitryed6263@gmail.com>
2025-05-20 22:01:10 +0400

    add GrassSpawn.java

---
+++ src/main/java/org/simulation/action/GrassSpawn.java
@@ -0,0 +1,13 @@
+package org.simulation.action;
+
+import org.simulation.core.Board;
+
+public class GrassSpawn implements Action{
+
+    @Override
+    public void execute(Board board) {
+        for (int col = 0; col < ; col++) {
+            
+        }
+    }
+}
