1e5c44bc7e339bd36eedd9f29c26df58bbb952c5
dmitry <dmitryed6263@gmail.com>
2025-05-26 23:19:56 +0400

    finally fucked this "makeMove" for herbivore

--- src/main/java/org/simulation/entity/Herbivore.java
+++ src/main/java/org/simulation/entity/Herbivore.java
@@ -11,6 +11,44 @@ public class Herbivore extends Creature {
 
         // стремится найти съедобный ресурс
         // может потратить ход на движение в сторону травы, либо на её поглощение
+        AStar aStar = new AStar(board);
+
+        List<Position> grassPositions = new ArrayList<>();
+
+        for(Position position : board.getEntities().keySet()){
+            Entity entity = board.getEntity(position);
+            if (entity instanceof Grass){
+                grassPositions.add(position);
+            }
+        }
+
+        if (grassPositions.isEmpty()){
+            moveRandomly(board);
+        }
+
+        Position closesGrass = null;
+        List<Position> shortestPath = null;
+
+        for (int count = 0; count < grassPositions.size(); count++) {
+            Position grassPos = grassPositions.get(count);
+            List<Position> path = aStar.find(this.getPosition(), grassPos);
+            if (path != null && !path.isEmpty()){
+                if (shortestPath == null || path.size() < shortestPath.size()){
+                    shortestPath = path;
+                    closesGrass = grassPos;
+                }
+            }
+        }
+
+        if (shortestPath.size() == 2 && shortestPath.get(1).equals(closesGrass)){
+            board.remove(closesGrass);
+            this.changeHealth(simulationConfig.getGrassNutrition());
+            return;
+        }
+
+        Position nextStep = shortestPath.get(1);
+        board.moveEntity(this.getPosition(), nextStep);
+    }
 
     private void moveRandomly(Board board) {
         List<Position> freeNeighbors = getFreePositions(board);
